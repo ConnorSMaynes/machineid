b4021314321264b177c9e1b61724baf260b1434c
Connor Sherwood Maynes <connormaynes@gmail.com>
2022-02-26 14:03:37 -0500

    add supported python versions

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from distutils.core import setup
 
 setup(
 	name='machineid',
-	version='1.0.0',
+	version='1.0.1',
 	description='Get a stable, unique, machine id for both VMs and computers.',
 	author='Connor Sherwood Maynes',
 	author_email='connormaynes@gmail.com',
 	url='https://github.com/ConnorSMaynes/machineid',
 	packages=['machineid'],
+	python_requires='>=2.5.3',
 )
